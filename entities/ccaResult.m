65372e4dff6fc9449982c503ee8e738ca0005e69
abragtim <timur.abragimovich@mild.blue>
2024-08-29 22:07:40 +0200

    Expand with NODDI.

--- entities/ccaResult.m
+++ entities/ccaResult.m
@@ -17,7 +17,7 @@ classdef ccaResult
                 metrics, 'UniformOutput',false);
             metricsSegments = cat(4, metricsSegments{:});
             metricsSegments(brainmaskSegment) = zscore(metricsSegments(brainmaskSegment), 0, 4);
-            combinedMetricImg = sum(bsxfun(@times, metricsSegments, reshape(obj.Coefficients, 1, 1, 1, 4)), 4);
+            combinedMetricImg = sum(bsxfun(@times, metricsSegments, reshape(obj.Coefficients, 1, 1, 1, length(obj.Coefficients))), 4);
 
             combinedMetricNii = metrics{1}.get();
             combinedMetricNii.img = combinedMetricImg;
